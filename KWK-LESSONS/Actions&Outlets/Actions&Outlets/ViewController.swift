3797f3dad72b223034530ec6da9e903e8a462c55
saigeff <73437918+saigeff@users.noreply.github.com>
2021-07-28 14:04:52 -0400

    Actions&Outlets

---
+++ KWK-LESSONS/Actions&Outlets/Actions&Outlets/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Actions&Outlets
+//
+//  Created by Saige Forbes on 7/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var appTitle: UILabel!
+    @IBOutlet weak var textFeild: UITextField!
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        
+    }
+
+}
+
