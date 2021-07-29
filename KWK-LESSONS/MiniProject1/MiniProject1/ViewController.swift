622d947fb10fcee667cce45cd48e3505fe488cf0
saigeff <73437918+saigeff@users.noreply.github.com>
2021-07-28 23:08:12 -0400

    MiniProject1

---
+++ KWK-LESSONS/MiniProject1/MiniProject1/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MiniProject1
+//
+//  Created by Saige Forbes on 7/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var pictureExample: UIImageView!
+    @IBOutlet weak var appTitle: UILabel!
+    @IBOutlet weak var factOne: UILabel!
+    @IBOutlet weak var factTwo: UILabel!
+    @IBOutlet weak var factThree: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        //code goes here
+    }
+    
+}
+
