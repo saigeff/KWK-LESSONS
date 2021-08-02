1f3a58100b71189b4161df987074e11bd3a752ba
saigeff <73437918+saigeff@users.noreply.github.com>
2021-08-02 09:30:39 -0400

    Add files via upload

---
+++ FInalProjectApp/FInalProjectAppTests/FInalProjectAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  FInalProjectAppTests.swift
+//  FInalProjectAppTests
+//
+//  Created by Saige Forbes on 7/31/21.
+//
+
+import XCTest
+@testable import FInalProjectApp
+
+class FInalProjectAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
